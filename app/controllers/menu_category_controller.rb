e3cf4f32f922c18dc760f53d33842245e98e7923
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-14 02:24:40 +0530

    update menu_category_controller add update and destroy function

--- app/controllers/menu_category_controller.rb
+++ app/controllers/menu_category_controller.rb
@@ -1,4 +1,51 @@
 class MenuCategoryController < ApplicationController
+  before_action :set_menu_category, only: %i[ show edit update destroy ]
+
   def index
+    @categories = MenuCategory.all
+  end
+
+  def search
+    name = params[:name]
+    unless name.nil?
+      @categories = MenuCategory.where("lower(name)  Like '" + "#{name.downcase}%'")
+    else
+      @categories = MenuCategory.all
+    end
+    render "index"
+  end
+
+  def create
+    name = params[:name]
+    @menu_category = MenuCategory.new(name: name)
+    unless @menu_category.save
+      flash[:error] = @menu_category.errors.full_messages.join(", ")
+    end
+    redirect_to menu_category_index_path
+  end
+
+  def edit
+  end
+
+  def update
+    @menu_category.name = menu_category_params["name"]
+    @menu_category.save
+    redirect_to menu_category_index_path
+  end
+
+  def destroy
+    @menu_category.destroy
+    redirect_to menu_category_index_path
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_menu_category
+    @menu_category = MenuCategory.find(params[:id])
+  end
+
+  def menu_category_params
+    params.require(:menu_category).permit(:name)
   end
 end
