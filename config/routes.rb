21403f47b30ea32a0961d8e172830d572527ceda
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-14 02:18:13 +0530

    update menuCategories Controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'menu_category/index'
+  get "/menu_category/search" => "menu_category#search", as: :search_menu_category
+  resources :menu_category
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
