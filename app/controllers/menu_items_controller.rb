f9acc035118a6f9957ae507ae22bd0f9c3452246
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-15 02:41:44 +0530

    add search for menuItems and pagination and css design

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,8 +2,18 @@ class MenuItemsController < ApplicationController
   def index
     item = params[:item_id]
     unless (item.nil?)
-      @pagy, @items = pagy(MenuItem.where(menu_category_id: params[:item_id]).order(:id), items: 6)
+      @pagy, @items = pagy(MenuItem.where(menu_category_id: item).order(:id), items: 6)
+    else
+      @pagy, @items = pagy(MenuItem.all.order(:id), items: 6)
     end
-    @pagy, @items = pagy(MenuItem.all.order(:id), items: 6)
+  end
+
+  def search
+    name = params[:name]
+    puts "\n\n\n#{params[:item_id]}v\n\n\n"
+    unless name.nil?
+      @pagy, @items = pagy(MenuItem.where("lower(name)  Like '" + "#{name.downcase}%'").order(:id), items: 6)
+    end
+    render "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/menu_category/search" => "menu_category#search", as: :search_menu_category
+  get "/menu_items/search" => "menu_items#search", as: :search_menu_item
   resources :menu_category
   resources :menu_items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
