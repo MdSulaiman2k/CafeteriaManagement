edb16c38fcdf7fee27e67ae179e9cdb278b14d7f
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-21 00:53:03 +0530

    update user stylesheet and add validation menu_items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,5 +2,7 @@ class MenuItem < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :status, presence: true
+
   belongs_to :menu_category
 end
