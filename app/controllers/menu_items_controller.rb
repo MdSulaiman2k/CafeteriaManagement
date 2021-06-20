f17032136e6c9bf5582a165255dc80013a650301
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-21 00:42:52 +0530

    update menu_items frontend

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,4 +1,6 @@
 class MenuItemsController < ApplicationController
+  before_action :set_menu_item, only: %i[ edit update destroy ]
+
   def index
     item = params[:item_id]
     unless (item.nil?)
@@ -10,7 +12,6 @@ class MenuItemsController < ApplicationController
 
   def search
     name = params[:name]
-    puts "\n\n\n#{params[:item_id]}v\n\n\n"
     unless name.nil?
       @pagy, @items = pagy(MenuItem.where("lower(name)  Like '" + "#{name.downcase}%'").order(:id), items: 6)
     end
@@ -21,11 +22,44 @@ class MenuItemsController < ApplicationController
     @menu_categories = MenuCategory.all
   end
 
+  def edit
+    [@menu_categories = MenuCategory.all, @menu_item]
+  end
+
+  def update
+    @menu_item.name = menu_item_params["name"]
+    @menu_item.price = menu_item_params["price"]
+    @menu_item.description = menu_item_params["description"]
+    @menu_item.menu_category_id = menu_item_params[:category]
+    @menu_item.save
+    redirect_to menu_items_path
+  end
+
   def create
-    menu_item = MenuItem.new(name: params[:name], description: params[:description], price: params[:price], menu_category_id: params[:category])
+    menu_item = MenuItem.new(name: params[:name], description: params[:description], price: params[:price], status: "Active", menu_category_id: params[:category])
     unless menu_item.save
-      flash[:error] = @menu_category.errors.full_messages.join(", ")
+      flash[:error] = menu_item.errors.full_messages.join(", ")
     end
     redirect_to menu_add_item_path
+  end
+
+  def statusupdate
+    id = params[:id]
+    status = params[:status] ? "Active" : "InActive"
+    menu_item = MenuItem.find(id)
+    menu_item.status = status
+    menu_item.save!
+    redirect_to menu_items_path
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_menu_item
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :price, :description, :category)
   end
 end
