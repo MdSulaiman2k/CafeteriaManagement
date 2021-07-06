1484b78eb4a7b758ae2eab1df91250b1d1e651fc
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-07-06 15:42:34 +0530

    update invoice

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -5,11 +5,11 @@ class InvoiceController < ApplicationController
     @pending = Order.where("status = 'pending'").count(:id)
     @delivered = Order.where("status = 'delivered'").count(:id)
     @cancel = Order.where("status = 'cancel'").count(:id)
+    @walkInCustomer = Order.where("address_id is NULL").count(:id)
     @dayTotal = Order.where("status != 'cancel'").where(order_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).sum(:totalvalue)
     @weekTotal = Order.where("status != 'cancel' and order_at >=? and order_at<=?", DateTime.now().beginning_of_week - 1.weeks, DateTime.now()).sum(:totalvalue)
     @monthTotal = Order.where("status != 'cancel' and order_at >=? and order_at<=?", DateTime.now().beginning_of_month - 1.months, DateTime.now()).sum(:totalvalue)
     @yearTotal = Order.where("status != 'cancel' and order_at >=? and order_at<=?", DateTime.now().beginning_of_year - 1.years, DateTime.now()).sum(:totalvalue)
     @overAll = Order.where("status != 'cancel'").sum(:totalvalue)
-    @highestsale = Order.where("status != 'cancel'").maximum(:totalvalue)
   end
 end
